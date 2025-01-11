da865cdf933943fbf7c65711366328b4e8174ed0
mrhee-ia <mcrispasague@gmail.com>
2025-01-11 17:37:14 +0800

    frontend discarded, cors edited

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ["http://127.0.0.1:5173", "http://localhost:5173", "https://cleanhub-frontend-vpd2.vercel.app/"],
 
     'allowed_origins_patterns' => [],
 
